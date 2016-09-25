2ac21a7064a65fff7e4e28afee13a55028eaedbf
nandu20 <rnprabhu2008@gmail.com>
2016-09-25 11:11:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});    
 
 app.get('/;ArticleName', function (req, res) {
       res.send(createTemplate(articles[articleName]));
@@ -97,6 +102,9 @@ app.get('/Article-two', function (req, res) {
 app.get('/Article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

55b553f9bac723727ffb3e5abd5b75fd39d5438f
nandu20 <rnprabhu2008@gmail.com>
2016-10-28 20:09:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/counter',function(req,res){
 });    
 
 
-app.get('/article-One', function (req, res) {
+app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });

1d0146ebb5b63c6e2f0e25ff56433d655bfdd328
nandu20 <rnprabhu2008@gmail.com>
2016-10-28 21:11:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var app = express();
 app.use(morgan('combined'));
 
   
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 /*var counter = 0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 }); */   
 
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 

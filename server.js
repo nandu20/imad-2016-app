ef9fdc768f5449df8e0661bf5cded4c4d632ba6c
nandu20 <rnprabhu2008@gmail.com>
2016-11-05 20:30:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/articleName', function (req, res) {
  res.send(createTemplate(articles[articleName]));
 });
     
-//app.get('/article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });

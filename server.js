4775f905d46e54d8b93eb7566053efe1bb9bca5a
nandu20 <rnprabhu2008@gmail.com>
2016-11-05 19:52:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database:"nandu20",
     host:"db.imad.hasura-app.io",
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:process.envDB_PASSWORD
 };
 
 var app = express();

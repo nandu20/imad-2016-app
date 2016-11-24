5b62fe36c713c43c9000be6989d3bb7e2913b82f
nandu20 <rnprabhu2008@gmail.com>
2016-11-24 21:34:31 +0530

    [imad-console] Updates login.js

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ else{
 attempt --;// Decrementing by one.
 alert("You have left "+attempt+" attempt;");
 // Disabling fields after 3 attempts.
-if( attempt == 0){
+if( attempt === 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;

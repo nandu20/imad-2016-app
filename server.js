53da32909ad4e47aa7915066c4c554bdf3711f93
nandu20 <rnprabhu2008@gmail.com>
2016-11-24 18:14:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 }); 
-
+var names=[];
 app.get('/submit-name',function(req,res){
     
     var name =res.params.name;

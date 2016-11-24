c826f64b5b71479d43abeccfe93f619ee5a97e66
nandu20 <rnprabhu2008@gmail.com>
2016-11-24 18:07:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick = function() {
        var list= '';
         for (var i=0;i<names.length;i++)
         {
-          list += '<li>' < names[i] > '</li>';
+          list += '<li>' + names[i] + '</li>';
            // list +=  names[i] +'<br>';
          }
 	    var ul= document.getElementById('namelist');

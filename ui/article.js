6250422e61904234ba04d569ee37de1329c5b628
nandu20 <rnprabhu2008@gmail.com>
2016-11-26 00:18:56 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -19,7 +19,7 @@ submit.onclick = function () {
     };
     var nameInput = document.getElementById('name');
 var name = nameInput.value;
-    request.open('GET','http://nandu20.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://nandu20.imad.hasura-app.io/article-one/submit-name?name='+name,true);
     
     request.send(null);
     

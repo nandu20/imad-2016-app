cabc834942119f0ffbdefcdef12c0ec203b1607f
nandu20 <rnprabhu2008@gmail.com>
2016-11-26 00:32:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/article-one/submit-name',function(req,res){
     names.push(name);
     res.send(JSON.stringify(names));
 });
+var names=[];
+app.get('/article-two/submit-name',function(req,res){
+    
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 app.get('/register', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'register.html'));
 

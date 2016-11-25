4bafd4928432baf25f060ccc0b2f48ee28597c10
nandu20 <rnprabhu2008@gmail.com>
2016-11-25 20:01:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', function (req, res) {
 
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     
     var name = req.query.name;
     names.push(name);

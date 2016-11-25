20cbd49bf8e0def71a38c54c35376b289e2d923c
nandu20 <rnprabhu2008@gmail.com>
2016-11-25 19:56:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 var pool = new Pool(config);
 
@@ -86,13 +93,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name/:name',function(req,res){
-    
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {

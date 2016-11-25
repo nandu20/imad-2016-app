0fbabc7183a2bdc8c9ccaeded30f1864b985135e
nandu20 <rnprabhu2008@gmail.com>
2016-11-25 20:19:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -168,8 +168,7 @@ return true;
 }  
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
      var request =new XMLHttpRequest();
@@ -188,6 +187,8 @@ submit.onclick = function () {
             }
         }
     };
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
     request.open('GET','http://nandu20.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
